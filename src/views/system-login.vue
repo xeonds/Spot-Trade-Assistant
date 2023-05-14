dca240d45a188bb622a4bd62fd7fafd697db1304
xeonds <1456506329@qq.com>
2023-05-14 23:11:09 +0800

    Enabled frontend verify code

--- src/views/system-login.vue
+++ src/views/system-login.vue
@@ -1,47 +1,25 @@
 <template>
   <el-container>
     <el-card class="login">
-      用户登录
-      <el-form
-        :model="formEl"
-        ref="form"
-        label-width="80px"
-        :inline="false"
-        :rules="rules"
-      >
+      <el-text class="login-text">用户登录</el-text>
+      <el-form :model="formEl" ref="form" label-width="80px" :inline="false" :rules="rules">
         <el-form-item label="用户名" prop="username">
-          <el-input
-            v-model="formEl.username"
-            placeholder="请输入用户名"
-          ></el-input>
+          <el-input v-model="formEl.username" placeholder="请输入用户名"></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
-          <el-input
-            v-model="formEl.password"
-            type="password"
-            placeholder="请输入密码"
-          ></el-input>
+          <el-input v-model="formEl.password" type="password" placeholder="请输入密码"></el-input>
         </el-form-item>
         <el-form-item label="验证码" prop="verify">
           <el-row :gutter="20">
             <el-col :span="12" :offset="0">
-              <el-input
-                v-model="formEl.verify"
-                placeholder="请输入验证码"
-              ></el-input
-            ></el-col>
+              <el-input v-model="formEl.verify" placeholder="请输入验证码"></el-input></el-col>
             <el-col :span="12" :offset="0">
-              <sIdentify
-                :identifyCode="codeofverify"
-                @click="changecode"
-              ></sIdentify>
+              <sIdentify :identifyCode="codeofverify" @click="changeCode"></sIdentify>
             </el-col>
           </el-row>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit(form)" :loading="isloading"
-            >登录</el-button
-          >
+          <el-button type="primary" @click="onSubmit(form)" :loading="isloading">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -54,9 +32,16 @@ import { useRouter } from 'vue-router'
 import { login } from '../http/api/utils'
 import loginStatusJudge from '../utils/loginStatus'
 const router = useRouter()
+const form = ref()
+let formEl = reactive({
+  username: '',
+  password: '',
+  verify: ''
+})
 let isloading = ref(false)
-// 生成验证码
 let codes = '1234567890'
+
+// 生成验证码
 const gencode = () => {
   let code = ''
   for (let i = 0; i < 4; i++) {
@@ -65,18 +50,12 @@ const gencode = () => {
   }
   return code
 }
-// // 验证码
+
+// 验证码
 let codeofverify = ref(gencode())
-const changecode = () => {
+const changeCode = () => {
   codeofverify.value = gencode()
 }
-
-const form = ref()
-let formEl = reactive({
-  username: '',
-  password: '',
-  verify: ''
-})
 
 const checkCode = (rule: any, value: any, callback: any) => {
   if (value !== codeofverify.value) {
@@ -88,11 +67,11 @@ const checkCode = (rule: any, value: any, callback: any) => {
 
 const rules = reactive({
   username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-  password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
-  // verify: [
-  //   { required: true, message: '请输入验证码', trigger: 'blur' },
-  //   { validator: checkCode, trigger: 'blur' }
-  // ]
+  password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+  verify: [
+    { required: true, message: '请输入验证码', trigger: 'blur' },
+    { validator: checkCode, trigger: 'blur' }
+  ]
 })
 
 const onSubmit = async (form: any) => {
@@ -139,13 +118,22 @@ if (loginStatusJudge()) {
   flex-flow: row;
   justify-content: center;
   align-items: center;
-}
 
-.login {
-  width: 24rem;
-  height: 16rem;
-  display: flex;
-  flex-flow: column;
-  justify-content: space-around;
+  .login {
+    width: 24rem;
+    display: flex;
+    flex-flow: column;
+    justify-content: space-around;
+
+    .login-text {
+      text-align: center;
+      font-size: 1.5rem;
+      margin: 0.5rem;
+    }
+
+    .el-form {
+      margin-top: 1rem;
+    }
+  }
 }
 </style>
