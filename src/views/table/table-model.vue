446606093ffb21793ebf9dce5235e62d444fafbf
xeonds <1456506329@qq.com>
2023-05-18 22:18:37 +0800

    UI fix & part of view: table-model

--- src/views/table/table-model.vue
+++ src/views/table/table-model.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="table-area">
-    <TableFind :search_item="search_item"></TableFind>
+  <div class="model">
     <Table
-      :contain_command="true"
-      :table_data="data"
+      class="model-box"
+      :col="table_col.UserInfo"
+      :table_data="model"
       :contain_top="true"
-      :property="property"
-      :command="command"
-      @handle="handle"
-      name="合同模板"
-      color="#e7e6e6"
-      :label="label"
-      :haveslot="true"
+      :contain_command="true"
+      :command="['添加', '编辑', '删除', '导出']"
+      :name="'模板'"
+      @handle="handle_fresh"
+      @menu="handle_fresh"
+      @load="handle_fresh"
+      @click_row="console.log('clicked')"
+      :hasfold="false"
+      :enable_select="false"
     >
-      <template #tr_extend>
-        <button style="margin-left: 1vw; font-size: 0.7vw">上传</button>
-        <button style="margin-left: 1vw; font-size: 0.7vw">查看</button>
+      <template #top>
+        <TableFind :search_item="search_item" class="table-find"></TableFind>
       </template>
     </Table>
   </div>
@@ -23,80 +24,32 @@
 
 <script lang="ts" setup>
 import Table from '../../components/main-table.vue'
-import { reactive } from 'vue'
 import TableFind from '../../components/table-find.vue'
+import { reactive } from 'vue'
+import * as table_col from '../../assets/table_info/table-title'
 
-let data = reactive([
-  {
-    m1: '内贸',
-    m2: '一般（固定值）',
-    m3: '现货',
-    m4: '购',
-    m5: '0.0003'
-  },
-  {
-    m1: '内贸',
-    m2: '一般（固定值）',
-    m3: '现货',
-    m4: '购',
-    m5: '0.0003'
-  },
-  {
-    m1: '内贸',
-    m2: '一般（固定值）',
-    m3: '现货',
-    m4: '购',
-    m5: '0.0003'
-  },
-  {
-    m1: '内贸',
-    m2: '一般（固定值）',
-    m3: '现货',
-    m4: '购',
-    m5: '0.0003'
-  },
-  {
-    m1: '内贸',
-    m2: '一般（固定值）',
-    m3: '现货',
-    m4: '购',
-    m5: '0.0003'
-  },
-  {
-    m1: '内贸',
-    m2: '一般（固定值）',
-    m3: '现货',
-    m4: '购',
-    m5: '0.0003'
-  },
-  {
-    m1: '内贸',
-    m2: '一般（固定值）',
-    m3: '现货',
-    m4: '购',
-    m5: '0.0003'
-  }
-])
 let search_item = reactive(['贸易类型', '订单模式', '交收方式', '购/销'])
-let label = reactive([
-  '贸易类型',
-  '订单模式',
-  '交收方式',
-  '购销方向',
-  '印花税率',
-  '合同模板'
-])
-let property = reactive(['m1', 'm2', 'm3', 'm4', 'm5'])
-let command = reactive(['添加', '删除', '修改'])
+let model = ref()
+let menus = ref()
 
-const handle = (a: number) => {
-  console.log(a)
+const handle_fresh = (name: string) => {
+  if (name == '用户管理') {
+    model.value.refresh_data()
+  }
+  if (name == '权限管理') {
+    menus.value.refresh_data()
+  }
 }
 </script>
 
-<style lang="less" scoped>
-.table-area {
-  margin: 0 auto;
-  width: 99vw;
+<style lang="less">
+.model {
+  .model-box {
+    margin-inline: 1rem !important;
+    margin-top: 1rem !important;
+  }
+}
+.table-find {
+  margin-bottom: 20px;
 }
 </style>
