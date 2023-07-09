d594a78cfd42e5161df71c149405ac58d5aa2798
xeonds <1456506329@qq.com>
2023-07-10 03:58:50 +0800

    Table-model progress: 3/4

--- src/views/table/table-model-new.vue
+++ src/views/table/table-model-new.vue
@@ -32,17 +32,78 @@
         </Table>
       </el-col>
     </el-row>
-    <el-dialog v-model="isShow" title="编辑">
-      <el-form>
-        <el-form-item
-          :label="value"
-          :label-width="200"
-          v-for="(index, value) in form_data"
-          :key="index"
-          :prop="value"
-        >
-          <el-input v-model="form_data[value]" />
+    <el-dialog
+      v-model="isShow"
+      :title="form_data['name'] + ' 基础信息'"
+      width="80%"
+    >
+      <el-form label-width="100">
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <el-form-item label="编号">
+              <el-input v-model="form_data.no" />
+            </el-form-item>
+            <el-form-item label="日期">
+              <el-date-picker
+                v-model="form_data.date"
+                type="date"
+                placeholder="选择日期"
+                :shortcuts="shortcuts"
+              ></el-date-picker>
+            </el-form-item>
+            <el-form-item label="签订地点">
+              <el-input v-model="form_data.place" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="甲方">
+              <el-input v-model="form_data.firstParty.name" />
+            </el-form-item>
+            <el-form-item label="甲方联系人">
+              <el-input v-model="form_data.firstParty.proxy" />
+            </el-form-item>
+            <el-form-item label="甲方电话">
+              <el-input v-model="form_data.firstParty.phone" />
+            </el-form-item>
+            <el-form-item label="甲方传真">
+              <el-input v-model="form_data.firstParty.fax" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="乙方">
+              <el-input v-model="form_data.secondParty.name" />
+            </el-form-item>
+            <el-form-item label="乙方联系人">
+              <el-input v-model="form_data.secondParty.preview" />
+            </el-form-item>
+            <el-form-item label="乙方电话">
+              <el-input v-model="form_data.secondParty.phone" />
+            </el-form-item>
+            <el-form-item label="乙方传真">
+              <el-input v-model="form_data.secondParty.fax" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row></el-row>
+        <el-form-item label="合同条款">
+          <el-row
+            v-for="(clause, index) in form_data.clauses"
+            :key="index"
+            style="width: 100%"
+          >
+            <div style="width: 100%">
+              <el-row>
+                <el-input v-model="form_data.clauses[index]" type="textarea" />
+              </el-row>
+              <el-row>
+                <el-button type="danger" plain @click="removeClause(index)"
+                  >删除</el-button
+                >
+              </el-row>
+            </div>
+          </el-row>
         </el-form-item>
+        <el-button type="primary" plain @click="addClause">添加条款</el-button>
       </el-form>
       <template #footer>
         <span class="dialog-footer">
@@ -63,6 +124,7 @@ import Table from '../../components/main-table.vue'
 import * as table_col from '../../assets/table_info/table-title'
 import * as table_add from '../../assets/table_info/table-add'
 import serviceAxios from '../../http'
+import download from '../../utils/download'
 
 export default {
   data() {
@@ -70,7 +132,65 @@ export default {
       name: '',
       isShow: false,
       data: [],
-      form_data: {},
+      label_data: {
+        no: '编号',
+        date: '日期',
+        location: '签订地点',
+        template: '模板内容',
+        place: '签订地点',
+        firstParty: '甲方',
+        secondParty: '乙方'
+      },
+      shortcuts: [
+        {
+          text: '今天',
+          value: new Date()
+        },
+        {
+          text: '昨天',
+          value: () => {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24)
+            return date
+          }
+        },
+        {
+          text: '一周前',
+          value: () => {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+            return date
+          }
+        }
+      ],
+      form_data: {
+        id: 1,
+        name: '',
+        categoryId: 1,
+        fileSrc: '',
+        firstParty: {
+          name: '',
+          proxy: null,
+          phone: null,
+          fax: null
+        },
+        secondParty: {
+          name: null,
+          proxy: null,
+          phone: null,
+          fax: null
+        },
+        date: '',
+        place: null,
+        no: null,
+        clauses: [
+          '一、经双方友好协商，货物名称、商标、型号、厂家、数量、价格、金额签订如上。',
+          '二、质量、验收标准：按照产品国家标准或参考生产厂商提供的质保单，如不符合，乙方有权要求甲方换货并承担全部相关费用。溢短装为2%，合理磅差为±2‰，以仓库方提供的磅码单为结算依据，若重量相差过大，以双方确认的第三方过磅为准，对超出合理磅差部分，双方同意多退少补。',
+          '三、结算方式及期限：本合同签订当日，乙方支付货款。甲方在收到货款当日向乙方提供提货单或仓单，自本合同签订一个月内甲方向乙方开具增值税发票。',
+          '四、交（提）货地点：【仓库简称】。运输方式：乙方自提，费用及运输风险由乙方自行承担。',
+          '五、权利瑕疵担保：甲方保证享有本协议项下的货物的所有权，任何第三方不得对该货物提出任何权利要求，否则由甲方承担全部责任。'
+        ]
+      },
       table_col,
       table_add,
       id: this.$route.params.id
@@ -91,6 +211,12 @@ export default {
     }
   },
   methods: {
+    addClause() {
+      this.form_data.clauses.push('')
+    },
+    removeClause(index) {
+      this.form_data.clauses.splice(index, 1)
+    },
     preview() {
       ElMessage({
         message: '开发中',
@@ -114,21 +240,16 @@ export default {
       const res = await serviceAxios.get(`/template/category`)
       this.name = res[this.id - 1].name
     },
-    downloadFile() {
-      const data = JSON.stringify(this.data)
-      const blob = new Blob([data], { type: 'application/json' })
-      const url = URL.createObjectURL(blob)
-      const link = document.createElement('a')
-      link.href = url
-      link.download = 'export.xls'
-      link.click()
-    },
     genearteTemplate() {
       serviceAxios
         .post(`/template/export/${this.form_data.id}`, this.form_data)
         .then((res) => {
           this.isShow = false
-          this.downloadFile(res)
+          download(
+            res,
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'export.xlsx'
+          )
           ElMessage.success('生成成功')
         })
         .catch((err) => {
