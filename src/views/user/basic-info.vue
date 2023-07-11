ee43b5789dc2586f56e99154130551b67a5a9883
xeonds <1456506329@qq.com>
2023-07-12 02:49:12 +0800

    Add edit box

--- src/views/user/basic-info.vue
+++ src/views/user/basic-info.vue
@@ -5,7 +5,7 @@
         <el-text class="section-title">基本信息</el-text>
       </el-col>
       <el-col :span="12" class="section-option">
-        <el-button type="primary" plain>修改</el-button>
+        <el-button type="primary" plain @click="isEdit = true">修改</el-button>
       </el-col>
     </el-row>
     <el-row class="row-content">
@@ -35,19 +35,73 @@
         </el-descriptions>
       </el-col>
     </el-row>
+    <el-dialog v-model="isEdit" title="编辑用户信息">
+      <el-form label-width="100">
+        <el-form-item label="用户名">
+          <el-input v-model="user_info.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="微信号">
+          <el-input v-model="user_info.wechat" />
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="user_info.email" />
+        </el-form-item>
+        <el-form-item label="手机号">
+          <el-input v-model="user_info.phonenumber" />
+        </el-form-item>
+        <el-form-item label="性别">
+          <el-input v-model="user_info.sex" />
+        </el-form-item>
+        <el-form-item label="创建时间">
+          <el-input v-model="user_info.createTime" />
+        </el-form-item>
+        <el-form-item label="更新时间">
+          <el-input v-model="user_info.updateTime" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button @click="cancelEdit">取 消</el-button>
+        <el-button type="primary" @click="confirmEdit">确 定</el-button>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { UserGet } from '../../http/api/user'
+import { UserGet, UserRefresh } from '../../http/api/user'
+import { ref } from 'vue'
 const user = localStorage.getItem('username')
+const isEdit = ref(false)
 let user_info: any = ref({})
 
-if (user) {
+const getUserInfo = () => {
   UserGet({
     username: user
   }).then((res) => {
     user_info.value = res.data[0]
   })
 }
+const cancelEdit = () => {
+  isEdit.value = false
+  getUserInfo()
+}
+const confirmEdit = () => {
+  UserRefresh(user, user_info.value).then((res) => {
+    if (res.code == 200) {
+      isEdit.value = false
+      getUserInfo()
+      ElMessage.success('修改成功')
+    } else {
+      ElMessage.error(res.msg)
+    }
+  })
+}
+
+if (user) getUserInfo()
 </script>
+
+<style lnag="less">
+.section-option {
+  text-align: right;
+}
+</style>
