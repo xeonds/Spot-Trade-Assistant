70f90b1ee048947cb8114a9fe0140d337c6e2fb2
xeonds <1456506329@qq.com>
2023-07-26 23:23:03 +0800

    Add tab bar to nav

--- src/views/system-main.vue
+++ src/views/system-main.vue
@@ -1,6 +1,7 @@
 <template>
   <el-scrollbar id="scroll-container">
     <HeadLine id="view-header" />
+    <el-row class="row3"> </el-row>
     <div id="view-content">
       <router-view></router-view>
     </div>
@@ -17,12 +18,39 @@
 <script setup lang="ts">
 // import bottomMarqueen from '../components/bottom-marqueen.vue'
 import HeadLine from '../components/head-line.vue'
+const editableTabsValue = ref('2')
+const editableTabs = ref([
+  {
+    title: 'Tab 1',
+    name: '1',
+    content: 'Tab 1 content'
+  },
+  {
+    title: 'Tab 2',
+    name: '2',
+    content: 'Tab 2 content'
+  }
+])
 const getDate = () => {
   let date = new Date()
   let year = date.getFullYear()
   let month = date.getMonth() + 1
   let day = date.getDate()
   return `${year}年${month}月${day}日`
+}
+const removeTab = (targetName: string) => {
+  const tabs = editableTabs.value
+  let activeName = editableTabsValue.value
+  if (activeName === targetName) {
+    tabs.forEach((tab, index) => {
+      if (tab.name === targetName) {
+        const nextTab = tabs[index + 1] || tabs[index - 1]
+        if (nextTab) {
+          activeName = nextTab.name
+        }
+      }
+    })
+  }
 }
 </script>
 
@@ -33,7 +61,12 @@ const getDate = () => {
 
   #view-header {
     overflow: hidden;
-    height: 8rem !important;
+    height: 6rem !important;
+  }
+
+  .row3 {
+    height: 2rem;
+    background-color: var(--el-color-secondary);
   }
 
   #view-content {
