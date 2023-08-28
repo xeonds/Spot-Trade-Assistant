a99d5deb8c13c20822835938196d3be0bb0c28aa
xeonds <1456506329@qq.com>
2023-08-28 17:11:16 +0800

    Change style & lint

--- src/views/table/table-trade.vue
+++ src/views/table/table-trade.vue
@@ -1,17 +1,7 @@
 <template>
-  <formPopmenu
-    :menu-list="menuList"
-    :position="position"
-    v-show="isVisible.clickMenu"
-    @menu="handleCtxMenu"
-  />
+  <formPopmenu :menu-list="menuList" :position="position" v-show="isVisible.clickMenu" @menu="handleCtxMenu" />
   <!-- 右键弹出框 -->
-  <el-dialog
-    v-model="isVisible.dialogDelete"
-    title="删除确认"
-    width="30%"
-    align-center
-  >
+  <el-dialog v-model="isVisible.dialogDelete" title="删除确认" width="30%" align-center>
     <span>是否确定要删除本条记录</span>
     <template #footer>
       <span class="dialog-footer">
@@ -23,49 +13,22 @@
     </template>
   </el-dialog>
   <!-- 修改框 -->
-  <form-dialog
-    width="40%"
-    v-model="isVisible.dialogUpdate"
-    title="修改"
-    align-center
-    :col="[{ label: '修改值', prop: 'value', type: 'string' }]"
-    @submit="handleUpdate"
-  />
+  <form-dialog width="40%" v-model="isVisible.dialogUpdate" title="修改" align-center
+    :col="[{ label: '修改值', prop: 'value', type: 'string' }]" @submit="handleUpdate" />
   <!-- 新增购买 -->
-  <form-dialog
-    width="80%"
-    v-model="isVisible.purchase"
-    :title="'采购'"
-    :col="Gouxiaojilu_local"
-    @close="isVisible.purchase = false"
-    @submit="(data) => purchase(data)"
-    @write="
-      (flag, val) => {
+  <form-dialog width="80%" v-model="isVisible.purchase" :title="'采购'" :col="Gouxiaojilu_local"
+    @close="isVisible.purchase = false" @submit="(data) => purchase(data)" @write="(flag, val) => {
         handle_load(flag, val)
       }
-    "
-  />
+      " />
   <!-- 销售确认 -->
-  <form-dialog
-    width="80%"
-    v-model="isVisible.sale"
-    title="销售确认"
-    :col="Xiaoshouqueren_local"
-    @close="isVisible.sale = false"
-    @submit="(data) => saleConfirm(data)"
-    @write="
-      (flag, val) => {
+  <form-dialog width="80%" v-model="isVisible.sale" title="销售确认" :col="Xiaoshouqueren_local"
+    @close="isVisible.sale = false" @submit="(data) => saleConfirm(data)" @write="(flag, val) => {
         handle_load(flag, val)
       }
-    "
-  />
+      " />
   <!-- 现货结算价 -->
-  <el-dialog
-    width="80%"
-    v-model="isVisible.form1"
-    title="现货结算价"
-    align-center
-  >
+  <el-dialog width="80%" v-model="isVisible.form1" title="现货结算价" align-center>
     <el-table :data="data['1-2-1']" style="width: 100%" max-height="500">
       <el-table-column prop="ledger" label="账套" width="180" />
       <el-table-column prop="ourdept" label="部门" width="180" />
@@ -73,19 +36,9 @@
       <el-table-column prop="grade" label="规格" width="180" />
       <el-table-column align="right">
         <template #default="scope">
-          <el-input
-            v-model="tempform1[scope.$index]"
-            placeholder="订单结算价"
-            style="margin-right: 1vw; width: 7vw"
-          />
-          <el-input
-            v-model="tempform2[scope.$index]"
-            placeholder="进出口结算算价"
-            style="margin-right: 1vw; width: 7vw"
-          />
-          <el-button size="small" @click="calculate1(scope.$index, scope.row)"
-            >确定</el-button
-          >
+          <el-input v-model="tempform1[scope.$index]" placeholder="订单结算价" style="margin-right: 1vw; width: 7vw" />
+          <el-input v-model="tempform2[scope.$index]" placeholder="进出口结算算价" style="margin-right: 1vw; width: 7vw" />
+          <el-button size="small" @click="calculate1(scope.$index, scope.row)">确定</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -104,14 +57,8 @@
       <el-table-column prop="type" label="币种" width="180" />
       <el-table-column align="right">
         <template #default="scope">
-          <el-input
-            v-model="tempform1[scope.$index]"
-            placeholder="汇率参考值"
-            style="margin-right: 1vw; width: 7vw"
-          />
-          <el-button size="small" @click="calculate2(scope.$index, scope.row)"
-            >确定</el-button
-          >
+          <el-input v-model="tempform1[scope.$index]" placeholder="汇率参考值" style="margin-right: 1vw; width: 7vw" />
+          <el-button size="small" @click="calculate2(scope.$index, scope.row)">确定</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -125,42 +72,19 @@
   </el-dialog>
 
   <!-- 生成合同 -->
-  <form-dialog
-    width="80%"
-    v-model="isVisible.generate"
-    title="生成合同"
-    :col="Shengchenghetong"
-    @close="isVisible.generate = false"
-    @submit="(data) => generateContract(data)"
-  />
+  <form-dialog width="80%" v-model="isVisible.generate" title="生成合同" :col="Shengchenghetong"
+    @close="isVisible.generate = false" @submit="(data) => generateContract(data)" />
 
   <!-- 印花税付款申请 -->
-  <form-dialog
-    width="80%"
-    v-model="isVisible.request"
-    title="印花税付款申请"
-    :col="Yinhuashui"
-    @close="isVisible.request = false"
-    @submit="(data) => ruquestYinhua(data)"
-  />
+  <form-dialog width="80%" v-model="isVisible.request" title="印花税付款申请" :col="Yinhuashui"
+    @close="isVisible.request = false" @submit="(data) => ruquestYinhua(data)" />
 
   <div v-if="route.params.id === '1'">
     <el-row>
       <el-col :span="24">
-        <modifyTable
-          :data="data['1-1']"
-          :command="['刷新', '采购', '发送成交确认']"
-          name="购销订单"
-          id="trade1"
-          :col="tableCol.TradeInfo"
-          :height="30"
-          :selectable="true"
-          @load="handlepageload('1-1')"
-          @handle="(a:number)=>handle('1-1', a)"
-          @menu="menu"
-          @select="handleSelect"
-          v-loading="isLoading['1-1']"
-        >
+        <modifyTable :data="data['1-1']" :command="['刷新', '采购', '发送成交确认']" name="购销订单" id="trade1"
+          :col="tableCol.TradeInfo" :height="30" :selectable="true" @load="handlepageload('1-1')"
+          @handle="(a: number) => handle('1-1', a)" @menu="menu" @select="handleSelect" v-loading="isLoading['1-1']">
           <template #top>
             <tableFind :form-data="data['1-1']" @submit="console.log" />
           </template>
@@ -169,22 +93,10 @@
     </el-row>
     <el-row>
       <el-col :span="24">
-        <modifyTable
-          :data="data['1-2']"
-          :command="['刷新', '销售', '现货结算价', '汇率', '导出']"
-          name="现货持仓"
-          id="trade2"
-          :col="tableCol.PositionInfo"
-          :hasfold="true"
-          :selectable="true"
-          :height="30"
-          @load="handlepageload('1-2')"
-          @handle="(a: number) => handle('1-2', a)"
-          @select="handleSelect"
-          @menu="menu"
-          @expand-change="(row:any) => get_detail(row)"
-          v-loading="isLoading['1-2']"
-        >
+        <modifyTable :data="data['1-2']" :command="['刷新', '销售', '现货结算价', '汇率', '导出']" name="现货持仓" id="trade2"
+          :col="tableCol.PositionInfo" :hasfold="true" :selectable="true" :height="30" @load="handlepageload('1-2')"
+          @handle="(a: number) => handle('1-2', a)" @select="handleSelect" @menu="menu"
+          @expand-change="(row: any) => get_detail(row)" v-loading="isLoading['1-2']">
           <template #top>
             <tableFind />
           </template>
@@ -197,20 +109,9 @@
   <div v-if="route.params.id === '2'">
     <el-row>
       <el-col :span="24">
-        <modifyTable
-          :data="data['2-1']"
-          :command="['刷新', '生成合同']"
-          name="购销订单"
-          id="trade1"
-          :col="tableCol.TradeInfo2"
-          :height="16"
-          :selectable="true"
-          @load="handlepageload('2-1')"
-          @handle="(a:number)=>handle('2-1', a)"
-          @menu="menu"
-          @select="handleSelect"
-          v-loading="isLoading['2-1']"
-        >
+        <modifyTable :data="data['2-1']" :command="['刷新', '生成合同']" name="购销订单" id="trade1" :col="tableCol.TradeInfo2"
+          :height="16" :selectable="true" @load="handlepageload('2-1')" @handle="(a: number) => handle('2-1', a)"
+          @menu="menu" @select="handleSelect" v-loading="isLoading['2-1']">
           <template #top>
             <tableFind :form-data="data['1-1']" @submit="console.log" />
           </template>
@@ -219,20 +120,9 @@
     </el-row>
     <el-row>
       <el-col :span="24">
-        <modifyTable
-          :data="data['2-2']"
-          :command="['刷新', '印花税付款申请', '合同归档']"
-          name="购销合同"
-          id="trade2"
-          :col="tableCol.Gouxiaohetong"
-          :height="16"
-          :selectable="true"
-          @load="handlepageload('2-2')"
-          @handle="(a: number) => handle('2-2', a)"
-          @menu="menu"
-          @select="handleSelect"
-          v-loading="isLoading['2-2']"
-        >
+        <modifyTable :data="data['2-2']" :command="['刷新', '印花税付款申请', '合同归档']" name="购销合同" id="trade2"
+          :col="tableCol.Gouxiaohetong" :height="16" :selectable="true" @load="handlepageload('2-2')"
+          @handle="(a: number) => handle('2-2', a)" @menu="menu" @select="handleSelect" v-loading="isLoading['2-2']">
           <template #top>
             <tableFind />
           </template>
@@ -241,18 +131,9 @@
     </el-row>
     <el-row>
       <el-col :span="24">
-        <modifyTable
-          :data="data['2-3']"
-          :command="['刷新']"
-          name="印花税付款申请"
-          id="trade3"
-          :col="tableCol.Yinhuashui"
-          :height="16"
-          @load="handlepageload('2-3')"
-          @handle="(a: number) => handle('2-3', a)"
-          @menu="menu"
-          v-loading="isLoading['2-3']"
-        >
+        <modifyTable :data="data['2-3']" :command="['刷新']" name="印花税付款申请" id="trade3" :col="tableCol.Yinhuashui"
+          :height="16" @load="handlepageload('2-3')" @handle="(a: number) => handle('2-3', a)" @menu="menu"
+          v-loading="isLoading['2-3']">
           <template #top>
             <tableFind />
           </template>
@@ -778,7 +659,7 @@ const calculate2 = (index: any, row: any) => {
   }
 }
 
-const get_detail = (row) => {
+const get_detail = (row: any) => {
   tradeAPI.get_check({ positionId: row.id }).then((res) => {
     console.log(res)
   })
@@ -805,7 +686,8 @@ const generateContract = (data: any) => {
   data.tradeIds = ids
   tradeAPI.postContract(data).then(() => {
     ElMessage({ message: '生成成功', type: 'success' })
-  })
+    handleRefresh('2-2')
+  }).catch((err) => ElMessage.error(err))
   isVisible.value.generate = false
 }
 
